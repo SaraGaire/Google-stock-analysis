20f56ac34a66349d9b16fb8bdf7257a34f5fb101
Sara.G <151964803+SaraGaire@users.noreply.github.com>
2025-11-12 22:22:03 +0100

    Create main App component with GoogleStockAnalysis

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GoogleStockAnalysis from "./components/GoogleStockAnalysis";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <GoogleStockAnalysis />
+    </div>
+  );
+}
+
+export default App;
